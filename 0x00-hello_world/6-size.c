adfb26d838d00e1fc302e011ffa64c42cd9b88f0
Bernard Muthee <murabern@gmail.com>
2022-09-11 19:47:45 -0700

    use printf

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 /**
  * main - print the string in the put function
  *
@@ -8,7 +7,7 @@
  * this program prints "Programming is like building a multilingual puzzle"
  * Return: 0
  */
-int main(void)
+ int main(void)
 {
 	char c;
 	int i;
@@ -16,11 +15,11 @@ int main(void)
 	long long lli;
 	float f;
 
-
 	printf("Size of a char: %ld byte(s)\n", sizeof(c));
 	printf("Size of an int: %ld byte(s)\n", sizeof(i));
-	printf("Size of a long int: %ld byte(S)\n", sizeof(li));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
-	Printf("Size of a float: %ld byte(s)\n", sizeof(f));
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }
+
