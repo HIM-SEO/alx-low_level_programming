157611c0408312d028e8dfcb36029885587cdee7
Bernard Muthee <murabern@gmail.com>
2022-09-11 19:07:58 -0700

    task last redo

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+
+
 /**
  * main - print the string in the put function
  *
@@ -13,6 +15,7 @@ int main(void)
 	long li;
 	long long lli;
 	float f;
+
 
 	printf("Size of a char: %ld byte(s)\n", sizeof(c));
 	printf("Size of an int: %ld byte(s)\n", sizeof(i));
