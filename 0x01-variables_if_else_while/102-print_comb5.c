8ab36d9f14166540c82fc22ff20bcf793ebbe654
Bernard Muthee <murabern@gmail.com>
2022-09-13 17:01:20 -0700

    attmpt 4

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,28 +13,24 @@ int main(void)
 		n2 = n1;
 		while(n2 <= 99)
 		{
-			n2 = n1;
-			while(n2 <= 99)
+			if (n2 != n1)
 			{
-				if (n2 != n1)
-				{
-					putchar((n1 / 10) + 48);
-					putchar((n1 % 10) + 48);
-					putchar(' ');
-					putchar((n2 / 10) + 48);
-					putchar((n2 % 10) + 48);
+				putchar((n1 / 10) + 48);
+				putchar((n1 % 10) + 48);
+				putchar(' ');
+				putchar((n2 / 10) + 48);
+				putchar((n2 % 10) + 48);
 
-					if(n1 != 98 || n2 != 98)
-					{
-						putchar(',');
-						putchar(' ');
-					}
+				if(n1 != 98 || n2 != 98)
+				{
+					putchar(',');
+					putchar(' ');
 				}
-				++n2;
 			}
-			++n1;
+			++n2;
 		}
-		putchar('\n');
-		return 0;
+		++n1;
 	}
+	putchar('\n');
+	return (0);
 }
