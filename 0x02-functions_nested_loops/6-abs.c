55a41dda9d61daf990109c90a5d69221fc8e6414
Bernard <murabern@gmail.com>
2022-09-17 15:07:24 -0700

    fix

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * _abs - fuction that computes the absolute value of an integer
  * @c: is the int that will use for the argument of the fuction
@@ -7,7 +7,7 @@
  */
 int _abs (int c)
 {
-	if (c>0 || c==0)
+	if (c > 0 || c == 0)
 	{
 		return (c);
 	}
