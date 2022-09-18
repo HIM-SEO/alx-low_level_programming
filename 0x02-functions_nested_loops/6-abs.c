aecc4e568a26dbf8aabea9fdd33d32d1fbaa6107
Bernard <murabern@gmail.com>
2022-09-18 12:28:11 -0700

    fix

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,17 +1,20 @@
-#include <stdio.h>
 #include "main.h"
-
+#include <stdio.h>
 /**
- * _abs - fuction that computes the absolute value of an integer
- * @c: is the int that will use for the argument of the fuction
+ * _abs - function that computes the absolute value of an integer
+ * @c:  is the int that will use for the argument of the function
  * Return: 0
  */
-int main _abs(int c)
+int main(void)
 {
-	if (c > 0 || c == 0)
-	{
-		return (c);
-	}
-		else
-		return (c * -1);
+	int r;
+	r= _abs(-1);
+	printf("%d\n", r);
+	r = _abs(0);
+	printf("%d\n", r);
+	r = _abs(1);
+	printf("%d\n", r);
+	r = _abs(-98);
+	printf("%d\n", r);
+	return (0);
 }
