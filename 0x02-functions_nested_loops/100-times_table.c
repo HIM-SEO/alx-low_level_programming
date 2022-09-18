a7cce99e827c41ef4baff1318b56929efd517463
Bernard <murabern@gmail.com>
2022-09-18 12:59:19 -0700

    f

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,30 +13,30 @@ void print_times_table(int n)
 	{
 		for (digit = 0; digit <= n; digit++)
 		{
-			_putchar('0');
+			putchar('0');
 
 			for (mult = 1; mult <= n; mult++)
 			{
-				_putchar(',');
-				_putchar(' ');
+				putchar(',');
+				putchar(' ');
 				result = digit * mult;
 				if (result <= 99)
-					_putchar(' ');
+					putchar(' ');
 
 				if (result <= 9)
-					_putchar(' ');
+					putchar(' ');
 				if (result >= 100)
 				{
-					_putchar((result / 100) + '0');
-					_putchar((result / 10) % 10 + '0');
+					putchar((result / 100) + '0');
+					putchar((result / 10) % 10 + '0');
 				}
 				else if (result <= 99 && result >= 10)
 				{
-					_putchar((result / 10) + '0');
+					putchar((result / 10) + '0');
 				}
-				_putchar((result % 10) + '0');
+				putchar((result % 10) + '0');
 			}
-			_putchar('\n');
+			putchar('\n');
 		}
 	}
 }
