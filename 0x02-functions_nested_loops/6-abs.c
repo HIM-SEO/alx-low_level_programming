a2d23ef13b05c0947ae4ee1e59381fcf18659257
Bernard <murabern@gmail.com>
2022-09-18 18:45:45 -0700

    fix

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,16 +1,14 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * main checks the code
- *
- * Return: Always 0.
+ * _abs -> return absolute value of a number
+ * @n: parameter
+ * Return: absolute value of a number
  */
-int _abs(void)
+int _abs(int n)
 {
-	if (c > 0 || c == 0)
-	{
-		return (c);
-	}
-	else
-		return (c * -1);
+	if (n < 0)
+
+		n = -n
+			return (n);
 }
