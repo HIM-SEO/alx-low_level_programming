279fefac0f9e29345bb9d10b3c78c3a0b9294e87
Bernard <murabern@gmail.com>
2022-09-19 04:37:49 -0700

    f

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,13 +5,17 @@
  */
 int main(void)
 {
-int arr[3]={0,1,0},n;
+	 int n1=1,n2=2,n3,i,number
+		 printf("98:");
+	  scanf("%d",&number);
+	  printf("\n%d %d",n1,n2);
+	  for(i=2;i<number;++i)
+	  {
+		  n3=n1+n2;
+		  printf(" %d",n3);
+		  n1=n2; 
+		  n2=n3;
+	  }
+	 return 0;
+} 
 
-n=10;//user input 
-
-// series formation
-for(int i=3;i<=n+3;i++)
-	printf("\n%d",arr[i%3]=arr[(i+1)%3]+arr[(i+2)%3]);
-
-return 0; 
-}
