6cdbdbcaf53e140d09c5f6ff596a57f217b9cd1a
Bernard Murage <murabern@gmail.com>
2022-12-15 05:43:28 -0800

    fixed16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,17 +5,35 @@
  */
 int main(void)
 {
-	 int n1=1,n2=2,n3,i,98
-		__printf("98:");
-	  scanf("%d",&98);
-	  printf("\n%d %d",n1,n2);
-	  for(i=2;i<98;++i)
+	unsigned long int i, j, k, j1, j2, k1, k2;
+	
+	j = 1;
+	k = 2;
+	printf("%lu", j);
+
+	for (i = 1; i < 91; i++)
 	  {
-		  n3=n1+n2;
-		  printf(" %d",n3);
-		  n1=n2; 
-		  n2=n3;
+		printf(", %lu", k);
+		k = k + j;
+		j = k - j;
 	  }
-	 return 0;
+	j1 = j / 1000000000;
+	j2 = j % 1000000000;
+	k1 = k / 1000000000;
+	k2 = k % 1000000000;
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", k1 + (k2 / 1000000000));
+		printf("%lu", k2 % 1000000000);
+		k1 = k1 + j1;
+		j1 = k1 - j1;
+		k2 = k2 + j2;
+		j2 = k2 - j2;
+	}
+
+	printf("\n");
+
+	return 0;
 } 
 
