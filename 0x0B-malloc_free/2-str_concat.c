b5b82fe74f2c87966eceebb58a34c195bfe32deb
Bernard Murage <murabern@gmail.com>
2023-01-10 09:22:08 -0800

    fixed

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,10 +2,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * str_concat - get ends of input and add together for size
- * @s1: input one to concat
- * @s2: input two to concat
- * Return: concat of s1 and s2
+ * str_concat - concatenates 2 strings.
+ * a NULL string is treated as an empty string.
+ * @s1: pointer to string.
+ * @s2: pointer to string.
+ *
+ * Return: pointer to newly allocated memory which 
+ * has s1, s2 and null byte.
+ * NULL on failure.
  */
 char *str_concat(char *s1, char *s2)
 {
@@ -20,14 +24,17 @@ char *str_concat(char *s1, char *s2)
 	len1 = 0;
 	while (s1[len1] != '\0')
 		len1++;
+	len2 = 0;
 	while (s2[len2] != '\0')
 		len2++;
+
 	size = len1 + len2;
 
 	nstr = malloc((sizeof(char) * size) + 1);
 	/*check if malloc was successful */
 	if (nstr == NULL)
 		return (NULL);
+
 	i = 0;
 	while (i < len1)
 	{
